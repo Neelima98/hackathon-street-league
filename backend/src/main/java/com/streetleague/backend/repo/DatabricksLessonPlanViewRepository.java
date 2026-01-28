bddf0b57a3407e37bfef41c5424dffac965eeada
Neelima98 <neelima.menon98@gmail.com>
2026-01-28 23:24:55 +0000

    Lesson Plan View Changes SQL Query

--- backend/src/main/java/com/streetleague/backend/repo/DatabricksLessonPlanViewRepository.java
+++ backend/src/main/java/com/streetleague/backend/repo/DatabricksLessonPlanViewRepository.java
@@ -1,5 +1,8 @@
 package com.streetleague.backend.repo;
 
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.context.annotation.Profile;
@@ -17,6 +20,29 @@ public class DatabricksLessonPlanViewRepository {
 
     public DatabricksLessonPlanViewRepository(NamedParameterJdbcTemplate namedJdbc) {
         this.namedJdbc = namedJdbc;
+    }
+
+    private Integer intOrNull(ResultSet rs, String col) throws SQLException {
+    BigDecimal bd = rs.getBigDecimal(col);
+    return bd == null ? null : bd.intValue();
+    }
+
+    private Long longOrNull(ResultSet rs, String col) throws SQLException {
+    BigDecimal bd = rs.getBigDecimal(col);
+    return bd == null ? null : bd.longValue();
+    }
+
+    private Boolean boolOrNull(ResultSet rs, String col) throws SQLException {
+    Object o = rs.getObject(col);
+    if (o == null) return null;
+    if (o instanceof Boolean b) return b;
+    if (o instanceof BigDecimal bd) return bd.intValue() != 0;  // sometimes booleans come as 0/1
+    if (o instanceof Number n) return n.intValue() != 0;
+    return Boolean.parseBoolean(o.toString());
+    }
+
+    private String str(ResultSet rs, String col) throws SQLException {
+    return rs.getString(col);
     }
 
     public List<LessonPlanView> getLessonPlans(String difficultyLevel, String skillName, String deliveryMethod) { 
@@ -109,50 +135,50 @@ public class DatabricksLessonPlanViewRepository {
     return namedJdbc.query(sql, p, (rs, rowNum) -> {
       LessonPlanView d = new LessonPlanView();
 
-      d.lesson_plan_id = (Integer) rs.getObject("lesson_plan_id");
+      d.lesson_plan_id = intOrNull(rs, "lesson_plan_id");
       d.lesson_code = rs.getString("lesson_code");
       d.lesson_title = rs.getString("lesson_title");
-      d.theme_id = (Integer) rs.getObject("theme_id");
-      d.product_type_id = (Integer) rs.getObject("product_type_id");
+      d.theme_id = intOrNull(rs, "theme_id");
+      d.product_type_id = intOrNull(rs,"product_type_id");
       d.difficulty_level = rs.getString("difficulty_level");
       d.delivery_method = rs.getString("delivery_method");
-      d.duration_minutes = (Integer) rs.getObject("duration_minutes");
-      d.max_participants = (Integer) rs.getObject("max_participants");
-      d.requires_equipment = (Boolean) rs.getObject("requires_equipment");
+      d.duration_minutes = intOrNull(rs,"duration_minutes");
+      d.max_participants = intOrNull(rs,"max_participants");
+      d.requires_equipment = boolOrNull(rs, "requires_equipment");
 
-      d.session_id = (Integer) rs.getObject("session_id");
-      d.session_cohort_id = (Integer) rs.getObject("session_cohort_id");
-      d.lead_staff_id = (Integer) rs.getObject("lead_staff_id");
-      d.support_staff_id = (Integer) rs.getObject("support_staff_id");
+      d.session_id = intOrNull(rs,"session_id");
+      d.session_cohort_id = intOrNull(rs,"session_cohort_id");
+      d.lead_staff_id = intOrNull(rs,"lead_staff_id");
+      d.support_staff_id = intOrNull(rs,"support_staff_id");
       d.start_time = String.valueOf(rs.getObject("start_time"));
-      d.actual_duration_minutes = (Integer) rs.getObject("actual_duration_minutes");
+      d.actual_duration_minutes = intOrNull(rs,"actual_duration_minutes");
       d.session_type = rs.getString("session_type");
       d.session_date = String.valueOf(rs.getObject("session_date"));
 
-      d.cohort_id = (Integer) rs.getObject("cohort_id");
+      d.cohort_id = intOrNull(rs,"cohort_id");
       d.cohort_code = rs.getString("cohort_code");
       d.day_of_week = rs.getString("day_of_week");
       d.session_time = rs.getString("session_time");
 
       d.product_type_name = rs.getString("product_type_name");
       d.product_code = rs.getString("product_code");
-      d.duration_weeks = (Integer) rs.getObject("duration_weeks");
+      d.duration_weeks = intOrNull(rs,"duration_weeks");
 
       d.theme_name = rs.getString("theme_name");
       d.theme_code = rs.getString("theme_code");
       d.theme_category = rs.getString("theme_category");
       d.theme_description = rs.getString("theme_description");
 
-      d.bridge_id = (Integer) rs.getObject("bridge_id");
-      d.skill_id = (Integer) rs.getObject("skill_id");
+      d.bridge_id = intOrNull(rs,"bridge_id");
+      d.skill_id = intOrNull(rs,"skill_id");
       d.skill_importance = rs.getString("skill_importance");
-      d.learning_hours = (Integer) rs.getObject("learning_hours");
+      d.learning_hours = intOrNull(rs,"learning_hours");
       d.target_proficiency_level = rs.getString("target_proficiency_level");
 
       d.skill_name = rs.getString("skill_name");
-      d.min_proficiency = (Integer) rs.getObject("min_proficiency");
-      d.skill_category_id = (Integer) rs.getObject("skill_category_id");
-      d.max_proficiency = (Integer) rs.getObject("max_proficiency");
+      d.min_proficiency = intOrNull(rs,"min_proficiency");
+      d.skill_category_id = intOrNull(rs,"skill_category_id");
+      d.max_proficiency = intOrNull(rs,"max_proficiency");
 
       d.category_name = rs.getString("category_name");
       d.category_code = rs.getString("category_code");
